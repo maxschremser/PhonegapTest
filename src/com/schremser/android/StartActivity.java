c1d4d6e9cf9385f43148492c6027c47f8d583282
Maximilian Schremser <maxi@schremser.com>
2012-11-13 17:24:21 +0100

    init project

---
+++ src/com/schremser/android/StartActivity.java
@@ -0,0 +1,16 @@
+package com.schremser.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class StartActivity extends DroidGap {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
